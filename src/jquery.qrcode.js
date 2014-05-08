861024be64fe7fae4559c2c152ec006012cca279
Tobias Bocanegra <tripod@adobe.com>
2014-05-08 16:45:40 -0700

    Fixing rounding issue in safari

--- src/jquery.qrcode.js
+++ src/jquery.qrcode.js
@@ -253,7 +253,7 @@
 		drawModules = function (qr, context, settings) {
 
 			var moduleCount = qr.moduleCount,
-				moduleSize = settings.size / moduleCount,
+				moduleSize = Math.round(settings.size / moduleCount),
 				fn = drawModuleDefault,
 				row, col;
 
